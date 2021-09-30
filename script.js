f22c2386b0c13c7ae05e91c9f5c8142d022067c5
Aroyan <aroyanbakti2@gmail.com>
2021-09-30 09:50:42 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementsByClassName('menu');
+
+hamburger.addEventListener('click', {
+  handleEvent: function (e) {
+    //Some Function to show up menu
+    console.log('Kusam');
+  },
+});
